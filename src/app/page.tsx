83c83e5650a9f2f9f4401c3251797c03092413f8
Rohit C <rcvium24@gmail.com>
2023-12-27 13:00:39 +0530

    Update files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,15 @@ export default function Home() {
   return (
     <body>
       <title>GIV3R</title>
-      <h1>GIV3R</h1>
+      <nav className="navbar">
+        <Image className="title" src={"/title.png"} width={600} height={300} alt="GIV3R"/>
+        <button className="home">HOME</button>
+        <button className="about">ABOUT</button>
+        <button className="events">EVENTS</button>
+        <button className="volunteer">VOLUNTEER</button>
+        <button className="login">LOGIN</button>
+        <button className="register">REGISTER</button>
+      </nav>
     </body>
   )
 }
