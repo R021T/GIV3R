7ecfb22a618bcfec8866ff9c6722e75e10ae046d
Rohit C <rcvium24@gmail.com>
2023-12-28 12:55:45 +0530

    Updated files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,21 +5,14 @@ export default function Home() {
     <body>
       <title>GIV3R</title>
       <nav className="navbar">
-        <Image className="title" src={"/title.png"} width={600} height={300} alt="GIV3R"/>
-        <button className="home">HOME</button>
+      <button className="home">HOME</button>
         <button className="about">ABOUT</button>
         <button className="events">EVENTS</button>
+        <Image className="title" src={"/title.png"} width={600} height={300} alt="GIV3R"/>
         <button className="volunteer">VOLUNTEER</button>
         <button className="login">LOGIN</button>
         <button className="register">REGISTER</button>
       </nav>
-      <Image className="heart" src={"/heart.png"} width={300} height={600} alt="Heart"/>
-      <div className="tagline">
-        <a className="tag">Spreading smiles,</a>
-        <br></br>
-        <a className="line"> without borders!</a>
-        <Image className="design" src={"/design.png"} width={250} height={350} alt="Heart"/>
-      </div>
     </body>
   )
 }
