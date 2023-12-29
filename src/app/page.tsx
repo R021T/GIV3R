6a1bd14830b90e338d1ec8d5dc0e1293987426f3
Rohit C <rcvium24@gmail.com>
2023-12-29 13:30:57 +0530

    Updates files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,10 +17,15 @@ export default function Home() {
         <a className="tagline">Spreading smiles, without borders!</a>
         <Image className="smile" src={"/smile.png"} width={1000} height={500} alt="Smile"/>
       </div>
-      <a>
+      <a href="#what">
         <div className="scroll"></div>
         <div className="dot"></div>
       </a>
+      <div className="what" id="what">
+        <p>What is<br/><b className="name">GIV3R</b>?</p>
+        <hr className="line"/>
+        <p className="flip">Flip to find out!</p>
+      </div>
     </body>
   )
 }
