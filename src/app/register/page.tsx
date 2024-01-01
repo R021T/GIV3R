8d79dca4b6929c07010567e47f9c098afae67b86
Rohit C <rcvium24@gmail.com>
2024-01-01 14:59:14 +0530

    Updated files

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,7 +1,14 @@
 import styles from "./register.module.css"
+import Image from "next/image"
 
 export default function Register() {
   return (
-    <h1 className={styles.name}>Register</h1>
+    <main>
+      <title>GIV3R</title>
+      <div className={styles.bar}></div>
+      <div className={styles.circle}>
+        <Image className={styles.logo} src={"/title.png"} width={500} height={250} alt="GIV3R"/>
+      </div>
+    </main>
   )
 }
