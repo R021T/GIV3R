386b565bca4040c0cb3425fc3532e1bff3604c5e
Rohit C <rcvium24@gmail.com>
2024-01-09 22:43:27 +0530

    Updated files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,26 @@
 import Image from "next/image"
+import Link from "next/link"
 
 export default function Home() {
   return (
     <body>
       <title>GIV3R</title>
       <nav className="navbar">
-        <a href="#image">
+        <Link href="#LetsSpreadSmiles">
           <button className="home">HOME</button>
-        </a>
-        <a href="#what">
+        </Link>
+        <Link href="#WeAreGIV3R">
           <button className="about">ABOUT</button>
-        </a>
+        </Link>
         <button className="events">EVENTS</button>
         <Image className="title" src={"/title.png"} width={600} height={300} alt="GIV3R"/>
         <button className="volunteer">VOLUNTEER</button>
         <button className="login">LOGIN</button>
-        <button className="register">REGISTER</button>
+        <Link href={"/register"}>
+          <button className="register">REGISTER</button>
+        </Link>
       </nav>
-      <div className="image" id="image">
+      <div className="image" id="LetsSpreadSmiles">
         <a className="tagline">Spreading smiles, without borders!</a>
         <Image className="smile" src={"/smile.png"} width={1000} height={500} alt="Smile"/>
       </div>
@@ -26,7 +29,7 @@ export default function Home() {
         <div className="dot"></div>
       </a>
       <div className="bar"></div>
-      <div className="what" id="what">
+      <div className="what" id="WeAreGIV3R">
         <div className="card">
           <div className="front">
             <p className="is">What
