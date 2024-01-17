23f2fdbe1d5882133c26e92bbff50d44dceace80
Rohit C <rcvium24@gmail.com>
2024-01-17 22:43:02 +0530

    Updated files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,6 @@ export default function Home() {
   return (
     <>
       <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         <title>GIV3R</title>
       </head>
       <body>
