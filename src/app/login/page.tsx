fd5aa33d5dd178d5b3906d7ab8f8807e63cbebb7
Rohit C <rcvium24@gmail.com>
2024-01-19 19:27:45 +0530

    Updated files

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,7 +4,7 @@ import Image from "next/image"
 export default function Register() {
   return (
     <main>
-      <title>GIV3R</title>
+      <title>GIV3R丨Login</title>
       <div className={styles.bar}></div>
       <div className={styles.circle}>
         <Image className={styles.logo} src={"/title.png"} width={500} height={250} alt="GIV3R"/>
---
+++ src/app/register/page.tsx
@@ -0,0 +1,49 @@
+import styles from "./register.module.css"
+import Image from "next/image"
+import Link from "next/link"
+
+export default function Register() {
+  return (
+    <>
+      <title>GIV3R丨Register</title>
+      <div className={styles.header}>
+        <div className={styles.bar}></div>
+        <h1 className={styles.heading}>Register</h1>
+      </div>
+      <div className={styles.content}>
+        <div className={styles.left}>
+          <div className={styles.high}>
+            <div className={styles.circle}>
+              <Image className={styles.logo} src={"/title.png"} width={500} height={250} alt="GIV3R"/>
+            </div>
+          </div>
+          <div className={styles.low}>
+            <p className={styles.log}>Already have an account?<br/><Link className={styles.link} href={"/login"}><b className={styles.in}>Login</b></Link></p>
+          </div>
+        </div>
+        <div className={styles.right}>
+          <div className={styles.column}>
+            <div className={styles.card}>
+              <div className={styles.top}>
+                <Image className={styles.image} src={"/ngo.png"} width={500} height={500} alt="NGO"/>
+              </div>
+              <div className={styles.bottom}>
+                <p className={styles.text}>Are you a NGO?</p>
+              </div>
+            </div>
+          </div>
+          <div className={styles.column}>
+            <div className={styles.card}>
+              <div className={styles.top}>
+                  <Image className={styles.image} src={"/donor.png"} width={500} height={500} alt="Donor"/>
+                </div>
+                <div className={styles.bottom}>
+                  <p className={styles.text}>Are you a Donor?</p>
+                </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
