798e65598a16aea3fb20af3fc78746257e833317
Rohit C <rcvium24@gmail.com>
2024-01-19 22:03:46 +0530

    Updated files

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -13,12 +13,14 @@ export default function Register() {
       <div className={styles.content}>
         <div className={styles.left}>
           <button className={styles.card}>
-            <div className={styles.top}>
-              <Image className={styles.image} src={"/ngo.png"} width={500} height={500} alt="NGO"/>
-            </div>
-            <div className={styles.bottom}>
-              <p className={styles.text}>Are you a NGO?</p>
-            </div>
+            <Link className={styles.link} href={"/register/ngo"}>
+              <div className={styles.top}>
+                <Image className={styles.image} src={"/ngo.png"} width={500} height={500} alt="NGO"/>
+              </div>
+              <div className={styles.bottom}>
+                <p className={styles.text}>Are you a NGO?</p>
+              </div>
+            </Link>
           </button>
         </div>
         <div className={styles.mid}>
@@ -33,12 +35,14 @@ export default function Register() {
         </div>
         <div className={styles.right}>
           <button className={styles.card}>
-            <div className={styles.top}>
-                <Image className={styles.image} src={"/donor.png"} width={500} height={500} alt="Donor"/>
-              </div>
-              <div className={styles.bottom}>
-                <p className={styles.text}>Are you a Donor?</p>
-              </div>
+            <Link className={styles.link} href={"/register/donor"}>
+              <div className={styles.top}>
+                  <Image className={styles.image} src={"/donor.png"} width={500} height={500} alt="Donor"/>
+                </div>
+                <div className={styles.bottom}>
+                  <p className={styles.text}>Are you a Donor?</p>
+                </div>
+              </Link>
           </button>
         </div>
       </div>
