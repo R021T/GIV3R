95d2cbb83a1b2066726d4a3d46ae31dbf1662783
Rohit C <rcvium24@gmail.com>
2024-03-31 14:44:33 +0530

    Updated files

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -11,45 +11,74 @@ export default async function Register() {
   }
   return (
     <>
-      <title>GIV3R丨Register</title>
-      <div className={styles.header}>
-        <div className={styles.bar}></div>
-        <h1 className={styles.heading}>Register</h1>
-      </div>
-      <div className={styles.content}>
-        <div className={styles.left}>
-          <button className={styles.card}>
-            <Link className={styles.link} href={"/register/ngo"}>
-              <div className={styles.top}>
-                <Image className={styles.image} src={"/ngo.png"} width={500} height={500} alt="NGO"/>
-              </div>
-              <div className={styles.bottom}>
-                <p className={styles.text}>Are you a NGO?</p>
-              </div>
-            </Link>
-          </button>
+      <div className={styles.canvas}>
+        <title>GIV3R丨Register</title>
+        <div className={styles.header}>
+          <h1 className={styles.heading}>Register</h1>
         </div>
-        <div className={styles.mid}>
-          <div className={styles.high}>
-            <div className={styles.circle}>
-              <Image className={styles.logo} priority={true} src={"/logo.png"} width={500} height={250} alt="GIV3R"/>
+        <div className={styles.content}>
+          <div className={styles.left}>
+            <div className={styles.high}>
+              <div className={styles.circle}>
+                <Image className={styles.logo} priority={true} src={"/logo.png"} width={500} height={250} alt="GIV3R"/>
+              </div>
+            </div>
+            <div className={styles.low}>
+              <p className={styles.log}>Already have an account?<br/><Link className={styles.link} href={"/login"}><b className={styles.in}>Login</b></Link></p>
             </div>
           </div>
-          <div className={styles.low}>
-            <p className={styles.log}>Already have an account?<br/><Link className={styles.link} href={"/login"}><b className={styles.in}>Login</b></Link></p>
+          <div className={styles.mid}>
+            <div className={styles.ngo}>
+              <button className={styles.card}>
+                <Link className={styles.link} href={"/register/ngo"}>
+                  <div className={styles.top}>
+                    <Image className={styles.image} priority={true} src={"/ngo.png"} width={500} height={500} alt="NGO"/>
+                  </div>
+                  <div className={styles.bottom}>
+                    <p className={styles.text}>Are you a NGO?</p>
+                  </div>
+                </Link>
+              </button>
+            </div>
+            <div className={styles.services}>
+              <button className={styles.card}>
+                <Link className={styles.link} href={"/register/services"}>
+                  <div className={styles.top}>
+                    <Image className={styles.image} priority={true} src={"/services.png"} width={500} height={500} alt="Services"/>
+                  </div>
+                  <div className={styles.bottom}>
+                    <p className={styles.text}>Are you a Service Provider?</p>
+                  </div>
+                </Link>
+              </button>
+            </div>
           </div>
-        </div>
-        <div className={styles.right}>
-          <button className={styles.card}>
-            <Link className={styles.link} href={"/register/donor"}>
-                <div className={styles.top}>
-                  <Image className={styles.image} src={"/donor.png"} width={500} height={500} alt="Donor"/>
-                </div>
-                <div className={styles.bottom}>
-                  <p className={styles.text}>Are you a Donor?</p>
-                </div>
-              </Link>
-          </button>
+          <div className={styles.right}>
+            <div className={styles.donor}>
+              <button className={styles.card}>
+                <Link className={styles.link} href={"/register/donor"}>
+                    <div className={styles.top}>
+                      <Image className={styles.image} priority={true} src={"/donor.png"} width={500} height={500} alt="Donor"/>
+                    </div>
+                    <div className={styles.bottom}>
+                      <p className={styles.text}>Are you a Donor?</p>
+                    </div>
+                  </Link>
+              </button>
+            </div>
+            <div className={styles.needy}>
+              <button className={styles.card}>
+                <Link className={styles.link} href={"/register/needy"}>
+                    <div className={styles.top}>
+                      <Image className={styles.image} priority={true} src={"/needy.png"} width={500} height={500} alt="needy"/>
+                    </div>
+                    <div className={styles.bottom}>
+                      <p className={styles.text}>Are you a Beneficiary?</p>
+                    </div>
+                  </Link>
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </>
