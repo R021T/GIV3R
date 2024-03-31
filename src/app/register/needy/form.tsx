1943af7def9e12f35f3f3c7cf304b885085aca09
Rohit C <rcvium24@gmail.com>
2024-03-31 19:55:23 +0530

    Updated files

--- src/app/register/needy/form.tsx
+++ src/app/register/needy/form.tsx
@@ -12,70 +12,7 @@ interface ApiResponse {
 }
 
 export default function Form() {
-  const [data, setData] = useState<ApiResponse | null>(null)
-  const [isLoading, setLoading] = useState(true)
-  const [error, setError] = useState<string | null>(null)
-
-  useEffect(() => {
-    fetch("/api/auth/register/needy")
-      .then((res) => res.json())
-      .then((apiData: ApiResponse) => {
-        setData(apiData)
-        setLoading(false)
-      })
-      .catch((error) => {
-        console.error("Error fetching data:", error)
-        setError("An error occurred while fetching data.")
-        setLoading(false);
-      })
-  }, [])
-
-  if (isLoading){
-    return (
-      <>
-        <div className={styles.canvas}>
-          <div className={styles.above}>
-            <Image className={styles.loading} priority={true} src={"/loading.png"} width={300} height={300} alt="Loading"/>
-          </div>
-          <div className={styles.below}>
-            <h1>Loading...</h1>
-          </div>
-        </div>
-      </>
-    )
-  }
-
-  if (error){
-    return (
-      <>
-        <div className={styles.canvas}>
-          <div className={styles.above}>
-            <Image className={styles.error} priority={true} src={"/error.png"} width={300} height={300} alt="Error"/>
-          </div>
-          <div className={styles.below}>
-            <h1>Error loading profile!</h1>
-          </div>
-        </div>
-      </>
-    )
-  }
-
-  if (!data){
-    return (
-      <>
-        <div className={styles.canvas}>
-          <div className={styles.above}>
-            <Image className={styles.missing} priority={true} src={"/missing.png"} width={300} height={300} alt="Missing"/>
-          </div>
-          <div className={styles.below}>
-            <h1>No profile data.</h1>
-          </div>
-        </div>
-      </>
-    )
-  }
-
-  const router=useRouter()
+    const router=useRouter()
     const handleSubmit=async (e:FormEvent<HTMLFormElement>)=>{
         e.preventDefault()
         const formData=new FormData(e.currentTarget)
@@ -100,60 +37,122 @@ export default function Form() {
             router.refresh()
         }
     }
+    
+    const [data, setData] = useState<ApiResponse | null>(null)
+    const [isLoading, setLoading] = useState(true)
+    const [error, setError] = useState<string | null>(null)
 
+    useEffect(() => {
+        fetch("/api/auth/register/needy")
+        .then((res) => res.json())
+        .then((apiData: ApiResponse) => {
+            setData(apiData)
+            setLoading(false)
+        })
+        .catch((error) => {
+            console.error("Error fetching data:", error)
+            setError("An error occurred while fetching data.")
+            setLoading(false);
+        })
+    }, [])
 
-  return (
-    <>
-      <form className={styles.enter} onSubmit={handleSubmit}>
-            <div className={styles.ngo}>
-                <label>NGO:</label>
-                <select multiple name="ngo" defaultValue={""}>
-                    <option value={""} disabled>Select NGO</option>
-                    {data?.data.map((ngo: any) => (
-                        <option key={ngo.id} value={ngo.id}>{ngo.name}</option>
-                    ))}
-                </select>
+    if (isLoading){
+        return (
+        <>
+            <div className={styles.canvas}>
+            <div className={styles.above}>
+                <Image className={styles.loading} priority={true} src={"/loading.png"} width={300} height={300} alt="Loading"/>
             </div>
-            <div className={styles.firstname}>
-                <label>First name :</label>
-                <input name="firstname" type="text" placeholder="No constraints" required/>
+            <div className={styles.below}>
+                <h1>Loading...</h1>
             </div>
-            <div className={styles.middlename}>
-                <label>Middle name :</label>
-                <input name="middlename" type="text" placeholder="Not compulsory"/>
             </div>
-            <div className={styles.lastname}>
-                <label>Last name :</label>
-                <input name="lastname" type="text" placeholder="Not compulsory"/>
+        </>
+        )
+    }
+
+    if (error){
+        return (
+        <>
+            <div className={styles.canvas}>
+            <div className={styles.above}>
+                <Image className={styles.error} priority={true} src={"/error.png"} width={300} height={300} alt="Error"/>
             </div>
-            <div className={styles.age}>
-                <label>Age :</label>
-                <input name="age" type="number" placeholder="No constraints" required/>
+            <div className={styles.below}>
+                <h1>Error loading profile!</h1>
             </div>
-            <div className={styles.email}>
-                <label>Email :</label>
-                <input name="email" type="email" placeholder="No constraints" required/>
             </div>
-            <div className={styles.phone}>
-                <label>Phone :</label>
-                <input name="phone" type="tel" pattern="[0-9]{10}" placeholder="10 digits" required/>
+        </>
+        )
+    }
+
+    if (!data){
+        return (
+        <>
+            <div className={styles.canvas}>
+            <div className={styles.above}>
+                <Image className={styles.missing} priority={true} src={"/missing.png"} width={300} height={300} alt="Missing"/>
             </div>
-            <div className={styles.wallet}>
-                <label>Wallet ID:</label>
-                <input name="wallet" type="text" placeholder="Not compulsory"/>
+            <div className={styles.below}>
+                <h1>No profile data.</h1>
             </div>
-            <div className={styles.username}>
-                <label>Username :</label>
-                <input name="username" type="text" maxLength={25} placeholder="Max length 25" required/>
             </div>
-            <div className={styles.password}>
-                <label>Password :</label>
-                <input name="password" type="password" maxLength={25} placeholder="No constraints" required/>
-            </div>
-            <div className={styles.submit}>
-                <input type="submit"/>
-            </div>
-        </form>
-    </>
-  )
+        </>
+        )
+    }
+
+    return (
+        <>
+        <form className={styles.enter} onSubmit={handleSubmit}>
+                <div className={styles.ngo}>
+                    <label>NGO:</label>
+                    <select multiple name="ngo" defaultValue={""}>
+                        <option value={""} disabled>Select NGO</option>
+                        {data?.data.map((ngo: any) => (
+                            <option key={ngo.id} value={ngo.id}>{ngo.name}</option>
+                        ))}
+                    </select>
+                </div>
+                <div className={styles.firstname}>
+                    <label>First name :</label>
+                    <input name="firstname" type="text" placeholder="No constraints" required/>
+                </div>
+                <div className={styles.middlename}>
+                    <label>Middle name :</label>
+                    <input name="middlename" type="text" placeholder="Not compulsory"/>
+                </div>
+                <div className={styles.lastname}>
+                    <label>Last name :</label>
+                    <input name="lastname" type="text" placeholder="Not compulsory"/>
+                </div>
+                <div className={styles.age}>
+                    <label>Age :</label>
+                    <input name="age" type="number" placeholder="No constraints" required/>
+                </div>
+                <div className={styles.email}>
+                    <label>Email :</label>
+                    <input name="email" type="email" placeholder="No constraints" required/>
+                </div>
+                <div className={styles.phone}>
+                    <label>Phone :</label>
+                    <input name="phone" type="tel" pattern="[0-9]{10}" placeholder="10 digits" required/>
+                </div>
+                <div className={styles.wallet}>
+                    <label>Wallet ID:</label>
+                    <input name="wallet" type="text" placeholder="Not compulsory"/>
+                </div>
+                <div className={styles.username}>
+                    <label>Username :</label>
+                    <input name="username" type="text" maxLength={25} placeholder="Max length 25" required/>
+                </div>
+                <div className={styles.password}>
+                    <label>Password :</label>
+                    <input name="password" type="password" maxLength={25} placeholder="No constraints" required/>
+                </div>
+                <div className={styles.submit}>
+                    <input type="submit"/>
+                </div>
+            </form>
+        </>
+    )
 }
