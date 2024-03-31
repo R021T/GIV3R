501804686ab33c242624ff2972acb0dc5d71bbe3
Rohit C <rcvium24@gmail.com>
2024-03-31 19:51:07 +0530

    Updated files

--- src/app/register/needy/form.tsx
+++ src/app/register/needy/form.tsx
@@ -1,9 +1,8 @@
 "use client"
+import { useRouter } from "next/navigation"
 import React, { useState, useEffect, FormEvent } from 'react'
 import styles from "./needy.module.css"
 import Image from "next/image"
-import Link from 'next/link'
-import { useRouter } from "next/navigation"
 
 interface ApiResponse {
   data: {
