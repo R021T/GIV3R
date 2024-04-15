39d1aed170d0f88fcbeff04a892e54c4609c3a50
Rohit C <rcvium24@gmail.com>
2024-04-15 23:31:25 +0530

    Updated files

--- src/app/api/campaigns/route.ts
+++ src/app/api/campaigns/route.ts
@@ -2,7 +2,7 @@ import { sql } from "@vercel/postgres"
 import { NextResponse } from "next/server"
 
 export async function GET(){
-    const response=await sql`select * from campaign where status='Active'`
+    const response=await sql`select * from campaign`
     const data=response.rows
     return NextResponse.json({data})
 }
